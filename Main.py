99b6d6c36118e7588f6d421dcaa4c7c3c7b60741
sadam2004 <sadammohammed@unomaha.edu>
2025-05-09 11:46:03 -0500

    final

---
+++ Main.py
@@ -0,0 +1,14 @@
+from wsgiref.util import application_uri
+
+from PyQt6.QtWidgets import QApplication
+
+from Logic import *
+
+def main():
+    application = QApplication([])
+    window = Logic()
+    window.show()
+    application.exec()
+
+if __name__ == "__main__":
+    main()
